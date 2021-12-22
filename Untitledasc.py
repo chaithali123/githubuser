fcf5ac503296735ef36e81575683b0de8db9df8e
chaithali123 <95960556+chaithali123@users.noreply.github.com>
2021-12-21 22:34:23 -0800

    Add files via upload

---
+++ Untitledasc.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+dic={} #empty dic
+for i in range(97,97+26): # range of ascii value from 97-123
+    dic[chr(i)] = i # map character to ascii value
+print(dic)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
